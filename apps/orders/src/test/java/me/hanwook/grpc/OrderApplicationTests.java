be048a91df2892db55fce5f2ac7feaa9e8683a51
Ryu <ryu@hanwook.me>
2022-08-24 13:39:13 +0900

    first commit

---
+++ apps/orders/src/test/java/me/hanwook/grpc/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hanwook.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
