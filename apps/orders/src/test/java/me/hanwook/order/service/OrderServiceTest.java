992c4427c806f1357c5c74d7f53ef70c60f1874f
Ryu <ryu@hanwook.me>
2022-09-27 18:03:57 +0900

    Polishing.

--- apps/orders/src/test/java/me/hanwook/order/service/OrderServiceTest.java
+++ apps/orders/src/test/java/me/hanwook/order/service/OrderServiceTest.java
@@ -1,13 +1,12 @@
 package me.hanwook.order.service;
 
 import me.hanwook.order.entity.Order;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
